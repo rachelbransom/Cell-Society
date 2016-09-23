d9e8ea23f35b2ea53a12b6d542deaa4dff9c39d1
Rachel Bransom <rnb11@duke.edu>
2016-09-23 18:03:22 -0400

    Made nested for loops to initialize elements of grid with their actors and respective cells

--- src/simulation/SimulationVisualizer.java
+++ src/simulation/SimulationVisualizer.java
@@ -1,22 +1,43 @@
 package simulation;
 
 import cellUtil.Grid;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.shape.Rectangle;
 
-class SimulationVisualizer {
-
+class SimulationVisualizer extends UX {
+	private int myGridSize;
+	private int myPixelSize;
 	private Grid myCellGrid;
 	private Rectangle[][] myRectGrid;
+	private Group root;
 	
-	Node showSimulation(){
-		return null;
+	public SimulationVisualizer (int gridPixelSize, Group rootToSet){
+		rootToSet = root;
+		myPixelSize = gridPixelSize;
+	}
+	
+	Group showSimulation(){
+		return root;
+	}
+	
+	void update(){
+		
 	}
 
-	void getCurrentGrid(Grid grid){
+	void setCurrentGrid(Grid grid){
 		myCellGrid = new Grid( grid );
+		myGridSize = grid.getSize();
+		
 	}
 	
-	void initRectGrid(){}
+	void initRectGrid(){
+		for (int i=0; i<myGridSize; i++){
+			for (int j=0; i<myGridSize; j++){
+				myRectGrid[i][j] = new Rectangle(myPixelSize/myGridSize,myPixelSize/myGridSize);
+				root.getChildren().add(myRectGrid[i][j]);
+			}
+		}
+	}
 	
 }
