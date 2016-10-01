d843e10944335bebc9254b49409b63dd81fb64c9
Rachel Bransom <rnb11@duke.edu>
2016-09-30 22:23:02 -0400

    removed layout and confirguration from file and moved to UXConfiguration:

--- src/cellsociety_team23/UX.java
+++ src/cellsociety_team23/UX.java
@@ -26,25 +26,17 @@ import simulation.SimulationController;
  *
  */
 
-public class UX {
+public class UX extends UXConfiguration{
 	
-	private final int TITLE_X = 10, TITLE_Y = 100, 
-			CONTROLS_START_X = 10, CONTROLS_START_Y = 150, CONTROLS_SPACING = 45,
-			INSTRUCTIONSX = 275, INSTRUCTIONSY = 350,
-			SLIDER_TEXT_X = CONTROLS_START_X + 50, 
-			SLIDER_MIN = 1, SLIDER_MAX = 10, SLIDER_DEFAULT = 3;
 
 	private Scene scene;
 	private Group root = new Group();
 	private Group gridRoot = new Group();
+	private Group graphRoot = new Group();
 	private Timeline animation;
-	private Button play, stop, step, reset;
-	private Slider slider;
-	private ComboBox<String> xmlComboBox;
-	private ComboBox<String> shapeComboBox;
-	private Text cellSocietyText, instructionsText, sliderText;
+	
 	private SimulationController simulationControl;	
-	private ResourceBundle myResources;
+	
 
 	public static final int FRAMES_PER_SECOND = 1;
 	private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
@@ -66,23 +58,25 @@ public class UX {
 	public Scene init() {
 		scene = new Scene(root, Main.XSIZE, Main.YSIZE, Color.BLACK);
 		scene.getStylesheets().add(CSS_FILE_NAME);
+		
 		buttonInit();
-		sliderInit();
-		xmlComboBoxInit();
-		shapeComboBoxInit();
-		displayInstructions();
-		displayTitle();
-		displaySliderText();
+		buttonActionInit();
+		
+		root.getChildren().addAll(getButtons());
+		root.getChildren().addAll(
+				sliderInit(),
+				xmlComboBoxInit(),
+				shapeComboBoxInit(),
+				displayInstructions(),
+				displayTitle(),
+				displaySliderText());
+		
 		root.getChildren().add(gridRoot);
+		
 		return scene;
 	}
 	
-	private void buttonInit() {
-		play = new Button(myResources.getString("PlayButton"));
-		stop = new Button(myResources.getString("StopButton"));
-		step = new Button(myResources.getString("StepButton"));
-		reset = new Button(myResources.getString("ResetButton"));
-
+	private void buttonActionInit(){
 		play.setOnAction((event) -> {
 			playSimulation();
 		});
@@ -95,11 +89,10 @@ public class UX {
 		reset.setOnAction((event) -> {
 			resetSimulation();
 		});
-		root.getChildren().addAll(setControlLayout(play, 0), setControlLayout(stop, CONTROLS_SPACING),
-				setControlLayout(step, CONTROLS_SPACING * 2), setControlLayout(reset, CONTROLS_SPACING * 3));
 	}
 
 	private void playSimulation() {
+		//root.getChildren().add(this.simulationControl.getSimulationChart());
 		double speedMultiplier = slider.getValue();
 		KeyFrame frame = new KeyFrame(Duration.seconds(SECOND_DELAY / speedMultiplier), e -> step());
 		animation = new Timeline();
@@ -117,6 +110,7 @@ public class UX {
 	private void step() {
 		if (simulationControl != null) {
 			advanceGridRoot();
+			
 		}
 		checkSpeed();
 	}
@@ -142,7 +136,7 @@ public class UX {
 		gridRoot = simulationControl.returnNextVisualGrid();
 		root.getChildren().add(gridRoot);
 	}
-
+	
 	private void checkSpeed() {
 		if (slider.isValueChanging() == true) {
 			stopSimulation();
@@ -150,25 +144,6 @@ public class UX {
 		}
 	}
 	
-	private String getFile(String chosenFileName) {
-		switch (chosenFileName) {
-		case ("SEGREGATION"):
-			return "Segregation.xml";
-		case ("PREDATOR-PREY"):
-			return "Wator.xml";
-		case ("FIRE"):
-			return "Fire.xml";
-		case ("GAME OF LIFE"):
-			return "Life.xml";
-		case ("CHOOSE XML FILE"):
-			return "NONE CHOSEN";
-		case ("NO SIMULATION TYPE"):
-			return ("NoSimulationType.xml");
-		case ("INVALID CELL STATE"):
-			return ("InvalidCellState.xml");
-		}
-		return null;
-	}
 	
 //	private String getShape(String chosenShape){
 //		switch(chosenShape) {
@@ -180,64 +155,4 @@ public class UX {
 //		}	
 //	}
 	
-	
-
-	/*----------------- Private / Helper Methods -----------------------------*/
-
-	private void xmlComboBoxInit() {
-		ObservableList<String> xmlOptions = FXCollections.observableArrayList(myResources.getString("Segregation"), 
-				myResources.getString("PredatorPrey"),myResources.getString("Fire"),myResources.getString("GameOfLife"));
-		xmlComboBox = new ComboBox<String>(xmlOptions);
-		xmlComboBox.setValue(myResources.getString("XMLComboBoxText"));
-		root.getChildren().add(setControlLayout(xmlComboBox, CONTROLS_SPACING * 5));
-	}
-
-	private void shapeComboBoxInit() {
-		ObservableList<String> shapeOptions = FXCollections.observableArrayList(myResources.getString("Square"), 
-				myResources.getString("Triangle"));
-		shapeComboBox = new ComboBox<String>(shapeOptions);
-		shapeComboBox.setValue(myResources.getString("ShapeComboBoxText"));
-		root.getChildren().add(setControlLayout(shapeComboBox, CONTROLS_SPACING * 6));
-		
-	}
-	
-	private void sliderInit() {
-		slider = new Slider(SLIDER_MIN, SLIDER_MAX, SLIDER_DEFAULT);
-		slider.setMajorTickUnit(1f);
-		root.getChildren().add(setControlLayout(slider, CONTROLS_SPACING * 4));
-	}
-
-	private void displayInstructions() {
-		instructionsText = new Text(INSTRUCTIONSX, INSTRUCTIONSY, myResources.getString("Instructions"));
-		instructionsText.getStyleClass().add("instructions");
-		root.getChildren().add(instructionsText);
-	}
-
-	private String getXMLComboBoxValue() {
-		return xmlComboBox.getSelectionModel().getSelectedItem();
-	}
-	
-	private String getShapeComboBoxValue() {
-		return shapeComboBox.getSelectionModel().getSelectedItem();
-	}
-
-	private void displaySliderText() {
-		sliderText = new Text(SLIDER_TEXT_X, CONTROLS_START_Y + CONTROLS_SPACING*4, myResources.getString("SliderText"));
-		sliderText.getStyleClass().add("slider");
-		root.getChildren().add(sliderText);
-	}
-
-	private void displayTitle() {
-		cellSocietyText = new Text(TITLE_X, TITLE_Y, myResources.getString("DisplayTitle"));
-		cellSocietyText.getStyleClass().add("title");
-		root.getChildren().add(cellSocietyText);
-	}
-
-	private Control setControlLayout(Control control, int layoutY) {
-		control.setLayoutX(CONTROLS_START_X);
-		control.setLayoutY(CONTROLS_START_Y + layoutY);
-		control.setFocusTraversable(false);
-		control.getStyleClass().add("control");
-		return control;
-	}
 }
